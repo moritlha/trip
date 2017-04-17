500b909df30741245b90d8feb28ef1c6d76f0d96
mmadisha <mmadisha@MMADISHA>
2017-04-17 22:47:19 +0200

    trips controller added

---
+++ HolidayTrip/Controllers/TripsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HolidayTrip.Models;
+
+namespace HolidayTrip.Controllers
+{
+    public class TripsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Trips
+        public ActionResult Index()
+        {
+            return View(db.Trips.ToList());
+        }
+
+        // GET: Trips/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // GET: Trips/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trips/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TripId,StartDate,EndDate,Destination,Comments")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                trip.TripId = Guid.NewGuid();
+                db.Trips.Add(trip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trip);
+        }
+
+        // GET: Trips/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trips/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TripId,StartDate,EndDate,Destination,Comments")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trip).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trip);
+        }
+
+        // GET: Trips/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Trip trip = db.Trips.Find(id);
+            db.Trips.Remove(trip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
